19f65816701265474a63270acff3d20e43edc4b2
Rahul Mitra <Rahul.Mitra@gmail.com>
2019-10-09 21:37:15 -0400

    Add slug and Author

--- blog/admin.py
+++ blog/admin.py
@@ -4,12 +4,19 @@ from . import models
 class PostAdmin(admin.ModelAdmin):
     list_display = (
     'title',
+    'author',
     'created',
     'updated'
     )
     search_fields = (
     'title',
+    'author__username',
+    'author__first_name',
+    'author__last_name',
     )
+    prepopulated_fields = {
+      'slug': ('title',)
+    }
     pass
 
 admin.site.register(models.Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,48 @@
+from django.conf import settings
 from django.db import models
 
 class Post(models.Model):
     """
     Represents a blog post
     """
+    DRAFT = 'draft'
+    PUBLISHED = 'published'
+    STATUS_CHOICES = [
+        (DRAFT, 'Draft'),
+        (PUBLISHED, 'Published')
+    ]
+
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,  # The Django auth user model
+        on_delete=models.PROTECT,  # Prevent posts from being deleted
+        related_name='blog_posts',  # "This" on the user model
+        null=True,
+    )
+
+    title = models.CharField(max_length=255)
+    status = models.CharField(
+         max_length =10,
+         choices=STATUS_CHOICES,
+         default = DRAFT,
+         help_text='Set to "Published" to make public'
+)
+
+    slug = models.SlugField(
+         null=True,
+         unique_for_date='published',
+    
+
+    )
     title = models.CharField(max_length=255)
     content = models.TextField()
     created = models.DateTimeField(auto_now_add=True)  # Sets on create
     updated = models.DateTimeField(auto_now=True)  # Updates on each save
+    published = models.DateTimeField(
+        null =True,
+        blank=True,
+        help_text ='the publishing date'
 
+    )
     class Meta:
         ordering = ['-created']
 
